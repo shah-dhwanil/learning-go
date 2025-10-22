ea6bf9ca27aab23ed3e8a6bc7424e7b9fcd57684
Dhwanil Shah <itzdhwanil@gmail.com>
2025-10-22 12:03:30 +0000

    Hello World again..

--- learn_go_with_tests/hello_world/hello_test.go
+++ learn_go_with_tests/hello_world/hello_test.go
@@ -2,9 +2,18 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	expected:= "Hello Dhwanil"
-	if got:=Hello("Dhwanil");got!=expected{
-		t.Errorf("Got %q but expected %q",got,expected)
+func assertEqual(t testing.TB, got,want string){
+	t.Helper() //marks this function as helper so that it can provide correct context in case a test case fails
+	if got != want {
+		t.Errorf("Assertion Failed:- Got:- %v but Required:- %v",got,want)
 	}
+
+}
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		assertEqual(t,Hello("Dhwanil"),"Hello Dhwanil");
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		assertEqual(t,Hello(""),"Hello World")
+	})
 }
